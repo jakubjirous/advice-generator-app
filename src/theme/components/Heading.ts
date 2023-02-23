8537eedc46f47fa7a6c915b78d7beb21cf640335
Jakub Jirous <info@jakubjirous.cz>
2023-02-23 14:20:49 -0800

    [theme] edit heading styling

--- src/theme/components/Heading.ts
+++ src/theme/components/Heading.ts
@@ -2,9 +2,27 @@ import { defineStyleConfig } from "@chakra-ui/react";
 
 const Heading = defineStyleConfig({
   baseStyle: {
-    color: "white",
     fontWeight: "bold",
+    lineHeight: "short",
     letterSpacing: "normal",
+    color: "botticelli.100",
+    _before: {
+      content: `"\\201C"`,
+    },
+    _after: {
+      content: `"\\201D"`,
+    },
+  },
+  sizes: {
+    sm: {
+      fontSize: "2xl",
+    },
+    md: {
+      fontSize: "3xl",
+    },
+  },
+  defaultProps: {
+    size: "sm",
   },
 });
 

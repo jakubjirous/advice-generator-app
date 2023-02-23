e42b2409de5c74bb32e52b068819de91618242d8
Jakub Jirous <info@jakubjirous.cz>
2023-02-22 17:59:01 -0800

    [theme] add shadows

--- src/theme/foundations/shadows.ts
+++ src/theme/foundations/shadows.ts
@@ -4,7 +4,7 @@ import { pxToRem } from "@/utils/unitConvertors";
  * https://chakra-ui.com/docs/styled-system/theme#alphas
  */
 const shadows = {
-  outline: `0 0 ${pxToRem(1)} ${pxToRem(2)} var(--chakra-colors-white)`,
+  outline: `0 0 ${pxToRem(1)} ${pxToRem(2)} var(--chakra-colors-bayoux-100)`,
 };
 
 export default shadows;

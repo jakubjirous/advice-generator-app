346cb46173995b9c32be0aeb4f7b187adce42142
Jakub Jirous <info@jakubjirous.cz>
2023-02-23 10:54:46 -0800

    [app] add useQueryErrorResetBoundary hook

--- src/app/(advice)/page.tsx
+++ src/app/(advice)/page.tsx
@@ -3,28 +3,26 @@
 import Loading from "@/app/(advice)/loading";
 import Advice from "@/components/Advice";
 import { Button } from "@chakra-ui/react";
-import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { useQueryErrorResetBoundary } from "@tanstack/react-query";
 import React, { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 export default function Page() {
+  const { reset } = useQueryErrorResetBoundary();
+
   return (
-    <QueryErrorResetBoundary>
-      {({ reset }) => (
-        <ErrorBoundary
-          onReset={reset}
-          fallbackRender={({ resetErrorBoundary }) => (
-            <div>
-              There was an error! react-query
-              <Button onClick={() => resetErrorBoundary()}>Try again</Button>
-            </div>
-          )}
-        >
-          <Suspense fallback={<Loading />}>
-            <Advice />
-          </Suspense>
-        </ErrorBoundary>
+    <ErrorBoundary
+      onReset={reset}
+      fallbackRender={({ resetErrorBoundary }) => (
+        <div>
+          There was an error!
+          <Button onClick={() => resetErrorBoundary()}>Try again</Button>
+        </div>
       )}
-    </QueryErrorResetBoundary>
+    >
+      <Suspense fallback={<Loading />}>
+        <Advice />
+      </Suspense>
+    </ErrorBoundary>
   );
 }

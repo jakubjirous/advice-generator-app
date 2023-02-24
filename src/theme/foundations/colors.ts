671b530aa45353cc62dc67cea9a9f372be1b6be0
Jakub Jirous <info@jakubjirous.cz>
2023-02-24 10:26:16 -0800

    [theme] move comments to swimm docs

--- src/theme/foundations/colors.ts
+++ src/theme/foundations/colors.ts
@@ -8,12 +8,12 @@ const colors = {
     100: "hsl(193, 38%, 86%)",
   },
   aquamarine: {
-    100: "hsl(150, 100%, 66%)", // button background
+    100: "hsl(150, 100%, 66%)",
   },
   bayoux: {
-    100: "hsl(217, 19%, 38%)", // quote color
-    500: "hsl(217, 19%, 24%)", // card background
-    900: "hsl(218, 23%, 16%)", // background
+    100: "hsl(217, 19%, 38%)",
+    500: "hsl(217, 19%, 24%)",
+    900: "hsl(218, 23%, 16%)",
   },
 };
 
